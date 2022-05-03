0e23aa67d4ea7c0d3a02c837998c74984bcd9f20
slashcobain4 <104869533+slashcobain4@users.noreply.github.com>
2022-05-03 16:34:07 -0500

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,29 @@
+print('TTTTT  SSS ')
+print('  T   S   S')
+print('  T   S    ')
+print('  T    SSS ')
+print('  T      S')
+print('  T   S   S')
+print('  T    SSS ')
+
+#Tyler Schultz
+#My favorite color is red
+print('')
+print('TTTTT')
+print('  T  ')
+print('  T  ')
+print('  T  ')
+print('  T  ')
+print('  T  ')
+print('  T  ')
+
+print( """
+ SSS 
+S   S
+S    
+ SSS 
+    S
+S   S
+ SSS 
+ """
+)
